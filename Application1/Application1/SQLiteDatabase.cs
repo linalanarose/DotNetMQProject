a2c193510ee4b5b93f85fc39cb4adced239b286a
bbai <bingbaiatisu@gmail.com>
2014-01-16 15:25:27 -0500

    Need to commit

--- Application1/Application1/SQLiteDatabase.cs
+++ Application1/Application1/SQLiteDatabase.cs
@@ -6,7 +6,31 @@ using System.Windows.Forms;
 
 public class SQLiteDatabase
 {
+    SQLiteConnection m_dbConnection;
+
 	public SQLiteDatabase()
 	{
+        SQLiteConnection.CreateFile("MyDatabase.sqlite");
 	}
+
+    public void connectToDataBase(){
+        m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+        m_dbConnection.Open();
+    }
+
+    public void executeSQL(String sql)
+    {
+        SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+        command.ExecuteNonQuery();
+    }
+
+    public void deleteOldestMessage()
+    {
+        string sql = "Delete FROM message ORDER BY order ASC LIMIT 1";
+        executeSQL(sql);
+        mCount--;
+    }
+
+
+
 }
