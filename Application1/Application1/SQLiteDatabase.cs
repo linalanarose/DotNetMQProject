94c8da127b850dba0879c4538cecffaa4f5783b5
Alana Lin <alin@openspan.com>
2014-01-16 14:44:48 -0500

    +db constructor and addMessage method

--- Application1/Application1/SQLiteDatabase.cs
+++ Application1/Application1/SQLiteDatabase.cs
@@ -6,7 +6,33 @@ using System.Windows.Forms;
 
 public class SQLiteDatabase
 {
-	public SQLiteDatabase()
-	{
-	}
+    static int mCount;
+    SQLiteConnection m_dbConnection;
+    public SQLiteDatabase()
+    {
+        SQLiteConnection.CreateFile("MessageDatabase.sqlite");
+        //make a database
+        m_dbConnection = new SQLiteConnection("Data Source=MessageDatabase.sqlite;Version=3;");
+        mCount = 0;
+        m_dbConnection.Open();
+        string sql = "create table messages (order int, message varchar())";
+        SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+        command.ExecuteNonQuery();
+        m_dbConnection.Close();
+    }
+        
+    static void Main()
+    {
+    }
+
+    void createMessage(String message)
+    {
+        m_dbConnection.Open();
+        string sql = "insert into messages (order, message) values (" + mCount + ", '" + message + "')";
+        SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+        command.ExecuteNonQuery();
+        mCount++;
+        m_dbConnection.Close();
+    }
+
 }
