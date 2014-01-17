4237adb6950ff8a6badea687c47e8773985af79e
bbai <bingbaiatisu@gmail.com>
2014-01-17 12:53:01 -0500

    Move methods from receiver to db, add delay, modify constructor

--- Application1/Application1/Properties/Settings.Designer.cs
+++ Application1/Application1/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Sender.Properties {
+namespace Database.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
--- Application1/Receiver/Receiver.cs
+++ Application1/Receiver/Receiver.cs
@@ -14,25 +14,5 @@ namespace Receiver
         {
             database = new SQLiteDatabase();
         }
-
-        public String[] receiveAllMsgs()
-        {
-            int rowCount = this.numOfMsgs();
-            int count = 0;
-            String[] ret = new String[rowCount];
-            SQLiteCommand command = new SQLiteCommand("SELECT * FROM messages", database.m_dbConnection);
-            SQLiteDataReader reader = command.ExecuteReader();
-            while (reader.Read()){
-                ret[count] = "" + reader["message"];
-                count++;
-            }
-            return ret;
-        }
-
-        private int numOfMsgs(){
-            SQLiteCommand command = new SQLiteCommand("SELECT COUNT(msgID) from messages", database.m_dbConnection);
-            int rowCount = Convert.ToInt32(command.ExecuteScalar());
-            return rowCount;
-        }
     }
 }
