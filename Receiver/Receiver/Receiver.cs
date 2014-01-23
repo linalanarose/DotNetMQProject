fb10667e723dc0e5d266928765f4f5711dd6e2d2
Alana Lin <alin@openspan.com>
2014-01-23 17:00:46 -0500

    removed unneeded unzip/zip methods in receiver class

--- Receiver/Receiver/Receiver.cs
+++ Receiver/Receiver/Receiver.cs
@@ -115,47 +115,6 @@ namespace Receiver
 				}
 
         }
-
-        private static byte[] Zip(string text)
-        {
-            byte[] buffer = System.Text.Encoding.Unicode.GetBytes(text);
-            MemoryStream ms = new MemoryStream();
-            using (System.IO.Compression.GZipStream zip = new System.IO.Compression.GZipStream(ms, System.IO.Compression.CompressionMode.Compress, true))
-            {
-                zip.Write(buffer, 0, buffer.Length);
-            }
-
-            ms.Position = 0;
-            MemoryStream outStream = new MemoryStream();
-
-            byte[] compressed = new byte[ms.Length];
-            ms.Read(compressed, 0, compressed.Length);
-
-            byte[] gzBuffer = new byte[compressed.Length + 4];
-            System.Buffer.BlockCopy(compressed, 0, gzBuffer, 4, compressed.Length);
-            System.Buffer.BlockCopy(BitConverter.GetBytes(buffer.Length), 0, gzBuffer, 0, 4);
-            return gzBuffer;
-        }
-
-        private static string UnZip(byte[] compressedText)
-        {
-            byte[] gzBuffer = compressedText;
-            using (MemoryStream ms = new MemoryStream())
-            {
-                int msgLength = BitConverter.ToInt32(gzBuffer, 0);
-                ms.Write(gzBuffer, 4, gzBuffer.Length - 4);
-
-                byte[] buffer = new byte[msgLength];
-
-                ms.Position = 0;
-                using (System.IO.Compression.GZipStream zip = new System.IO.Compression.GZipStream(ms, System.IO.Compression.CompressionMode.Decompress))
-                {
-                    zip.Read(buffer, 0, buffer.Length);
-                }
-
-                return System.Text.Encoding.Unicode.GetString(buffer, 0, buffer.Length);
-            }
-        }
         /// <summary>
         /// Configure parameters of receiver and create instance of database
         /// </summary>
